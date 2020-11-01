a43ecdf768baa4fa7ff6f11ccf2f5ac90702a312
Jeffrey Chiu <mr.jeffrey.chiu@gmail.com>
2020-11-01 22:43:41 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,7 +6,6 @@ def find_min_in_nested_arrays(src)
   row_index = 0 
   while row_index < src.length do
     element_index = 0 
-    lowest_temperature = ""
     while element_index < src[row_index].length do
       if src[row_index][element_index] < lowest_temperature
       lowest_temperature = src[row_index][element_index]
